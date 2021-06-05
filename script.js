fecc0d4ec3633ee895825e6ad71422bc37e7a78b
Manojkumar Murugan <manojkumarnov23@gmail.com>
2021-06-05 18:44:21 +0530

    Image and style corrections

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function displayKural(){
     let container = document.querySelector(".container");
-    let kuralNumber = container.querySelector("#kuralNumber").value;
-    let kuralData = getKuralData(kuralNumber);
+    let kuralNumber = Number(container.querySelector("#kuralNumber").value),isValid=true;
+    if(kuralNumber===""){
+        alert("ஏதேனும் குறள் எண்ணை உள்ளீடவும்");
+        isValid=false;
+    } else if(kuralNumber<1 || kuralNumber>1330){
+        alert("குறள் எண்ணை சரி பார்க்கவும்");
+        container.querySelector("#kuralNumber").value="";
+        isValid=false;
+    }
+    if(!isValid) return false;
+    let kuralData = getKuralData(kuralNumber-1);
     let kuralDOM = container.querySelector(".kuralDOM");
     kuralDOM.innerHTML = "";
-    kuralDOM.innerHTML = ("<div><div>"+kuralData.Line1+"<div>"+kuralData.Line2+"</div></div>");
+    kuralDOM.innerHTML = ("<div><div>குறள் எண்:"+kuralData.Number+"</div><div>"+kuralData.Line1+"<div>"+kuralData.Line2+"</div><div><ul>கலைஞர் உரை</ul><br>"+kuralData.mk+"</div></div>");
 }
