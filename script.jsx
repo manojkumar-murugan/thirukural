6c4a85ddff907429d6c5b9e822b9de3e309ae8c9
Manojkumar Murugan <manojkumarnov23@gmail.com>
2021-06-05 13:28:58 +0530

    removed jsx

--- script.jsx
+++
@@ -1,8 +0,0 @@
-function displayKural(){
-    let container = document.querySelector(".container");
-    let kuralNumber = container.querySelector("#kuralNumber").value;
-    let kuralData = getKuralData(kuralNumber);
-    let kuralDOM = container.querySelector(".kuralDOM");
-    kuralDOM.innerHTML = "";
-    kuralDOM.innerHTML = ("<div><div>"+kuralData.Line1+"<div>"+kuralData.Line2+"</div></div>");
-}
